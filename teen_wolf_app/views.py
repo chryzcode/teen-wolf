53baa0f4b10085c917c3489fd3895b6a3fff32b9
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-12-13 16:07:02 +0100

    commit

--- teen_wolf_app/views.py
+++ teen_wolf_app/views.py
@@ -9,6 +9,6 @@ def index(request):
         subject = request.POST['name']
         message = request.POST['message']
         from_email = request.POST['email']
-        reply_to = [from_email]
+        reply_to = request.POST['email']
         send_mail(subject, message, from_email, ['contact@teenwolftoken.com'], fail_silently=False)
     return render(request, 'index.html')
