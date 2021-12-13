4bb4182e163ccd58b2fae5a2c86a58fb24af0440
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-12-13 16:12:52 +0100

    commit

--- teen_wolf_app/views.py
+++ teen_wolf_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .forms import ContactForm
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.conf import settings
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        subject = request.POST['name']
-        message = request.POST['message']
-        from_email = request.POST['email']
-        reply_to = request.POST['email']
-        send_mail(subject, message, from_email, ['contact@teenwolftoken.com'], fail_silently=False)
+        email =  EmailMessage(
+        subject = request.POST['name'],
+        message = request.POST['message'],
+        from_email = request.POST['email'],
+        reply_to = request.POST['email'],
+        )
+        sent = email.send (subject, message, from_email, ['contact@teenwolftoken.com'], fail_silently=False)
     return render(request, 'index.html')
